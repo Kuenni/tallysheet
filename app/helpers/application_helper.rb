cb792d8cf2af88a3ba3e5bf58bc67a888647ea1c
Andreas Kuensken <andreas@kuensken-online.de>
2025-12-30 07:25:24 +0000

    Change Styling of flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
 
   def flash_class(level)
     case level
-    when "notice" then "info"
+    when "notice" then "primary"
     when "success" then "success"
     when "error" then "danger"
     when "alert" then "danger"
